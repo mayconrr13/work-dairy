295b4c934580549eb52393574d243ac7f2502bd7
Maycon <mayconrr13@gmail.com>
2023-07-24 20:49:43 -0300

    feat: added project custom theme

---
+++ src/theme/customTheme.ts
@@ -0,0 +1,24 @@
+export const customTheme = {
+  colors: {
+    primary300: '#ACBFA4',
+    primary500: '#246A73',
+    primary600: '#1A535C',
+    danger500: '#EF271B',
+    danger700: '#9B2915',
+    info500: '#297FCE',
+    white: '#FFFFFF',
+    gray200: '#E5E5E5',
+    gray500: '#5C5C5C',
+    gray700: '#363636',
+    gray800: '#242424',
+    gray900: '#121212',
+    black: '#000000',
+  },
+  font: {
+    regular: 'PoppinsRegular',
+    semiBold: 'PoppinsSemiBold',
+    light: 'PoppinsLight',
+  },
+};
+
+export type CustomTheme = typeof customTheme;
