e2405b237c35d2b04e9e212c3b37d1d604ab32a2
Maycon <mayconrr13@gmail.com>
2023-07-24 20:51:58 -0300

    feat: added external fonts and updated App.tsx

--- App.tsx
+++ App.tsx
@@ -1,20 +1,40 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StackRoutes } from './src/routes/stack.routes';
+import { NavigationContainer } from '@react-navigation/native';
+import { useCallback } from 'react';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import KeyboardSafeZone from './src/components/KeyboardSafeZone';
+import React from 'react';
+import GlobalThemeProvider from './src/theme/GlobalThemeProvider';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    PoppinsLight: require('./src/assets/fonts/PoppinsLight.ttf'),
+    PoppinsRegular: require('./src/assets/fonts/PoppinsRegular.ttf'),
+    PoppinsSemiBold: require('./src/assets/fonts/PoppinsSemiBold.ttf'),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer onReady={onLayoutRootView}>
+      <GlobalThemeProvider>
+        <KeyboardSafeZone>
+          <StackRoutes />
+        </KeyboardSafeZone>
+        <StatusBar style="auto" />
+      </GlobalThemeProvider>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
